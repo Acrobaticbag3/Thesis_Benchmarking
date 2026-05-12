e5b4ff6ab0dbb04af0bdcb6c9a7f77ddace676e1
Kevin Johansson <kevintom214@gmail.com>
2026-05-12 12:40:25 +0200

    Experimenting with setup for runner script

---
+++ runner/run-all.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail   # e - exit emedietly on fail
+                    # u - undefined vars = failure instead of empty
+                    # o pipefail - full pipe failure if any operation in said pipe fails
+
+RESULTS="runner/results.csv"
+# echo "tool_name, test_round, time_to_deploy, time_to_rollback, test_result" > "$RESULTS"
+
+TOOLS=("helm", "kustomize", "timoni", "cdk8s")
+ROUNDS=10
+
+for tool in "${TOOLS[@]}"; do 
+  for round in $(seq 1 $ROUNDS); do
+    echo "${round}"
+  done
+done 
